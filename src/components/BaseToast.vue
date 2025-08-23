c2e305ef433457408f29ecff99ceea659c491b37
Gianfranco <gfmmortera@gmail.com>
2025-08-24 01:49:01 +0800

    feat: Add toast component

---
+++ src/components/BaseToast.vue
@@ -0,0 +1,32 @@
+<template>
+  <Teleport to="body">
+    <div
+      v-if="conditionalValue"
+      class="toast toast-top toast-center"
+    >
+      <div class="alert alert-error">
+        <span class="text-white">{{ message }}</span>
+        <button
+          class="btn btn-xs"
+          @click="$emit('click')"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </Teleport>  
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  conditionalValue: boolean
+  message: string | null
+}
+
+interface Emits {
+  'click': []
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
